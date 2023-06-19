adb0bc26788cfdaef313ec75515bb8d35d9cdac1
NateNats <nicolaussagraha14@gmail.com>
2023-06-19 16:11:36 +0700

    pembaruan untuk table mobil dan motor. ada sebuah pengisian data untuk setiap table nya.

--- motor/window.py
+++ motor/window.py
@@ -19,57 +19,57 @@ class App(Tk):
         self.resizable(False, False)
 
     def load_images(self):
-        self.background_img = PhotoImage(file="background.png")
-        self.img0 = PhotoImage(file="img0.png")
-        self.img1 = PhotoImage(file="img1.png")
-        self.img2 = PhotoImage(file="img2.png")
-        self.img3 = PhotoImage(file="img3.png")
-        self.img4 = PhotoImage(file="img4.png")
-        self.img5 = PhotoImage(file="img5.png")
-        self.img6 = PhotoImage(file="img6.png")
-        self.img7 = PhotoImage(file="img7.png")
-        self.img8 = PhotoImage(file="img8.png")
-        self.img9 = PhotoImage(file="img9.png")
-        self.img10 = PhotoImage(file="img10.png")
-        self.entry0_img = PhotoImage(file="img_textBox0.png")
-        self.entry1_img = PhotoImage(file="img_textBox1.png")
-        self.entry2_img = PhotoImage(file="img_textBox2.png")
-        self.entry3_img = PhotoImage(file="img_textBox3.png")
-        self.entry4_img = PhotoImage(file="img_textBox4.png")
-        self.entry5_img = PhotoImage(file="img_textBox5.png")
+        self.background_img = PhotoImage(file="background.png", master=self)
+        self.img0 = PhotoImage(file="img0.png", master=self)
+        self.img1 = PhotoImage(file="img1.png", master=self)
+        self.img2 = PhotoImage(file="img2.png", master=self)
+        self.img3 = PhotoImage(file="img3.png", master=self)
+        self.img4 = PhotoImage(file="img4.png", master=self)
+        self.img5 = PhotoImage(file="img5.png", master=self)
+        self.img6 = PhotoImage(file="img6.png", master=self)
+        self.img7 = PhotoImage(file="img7.png", master=self)
+        self.img8 = PhotoImage(file="img8.png", master=self)
+        self.img9 = PhotoImage(file="img9.png", master=self)
+        self.img10 = PhotoImage(file="img10.png", master=self)
+        self.entry0_img = PhotoImage(file="img_textBox0.png", master=self)
+        self.entry1_img = PhotoImage(file="img_textBox1.png", master=self)
+        self.entry2_img = PhotoImage(file="img_textBox2.png", master=self)
+        self.entry3_img = PhotoImage(file="img_textBox3.png", master=self)
+        self.entry4_img = PhotoImage(file="img_textBox4.png", master=self)
+        self.entry5_img = PhotoImage(file="img_textBox5.png", master=self)
 
     def create_buttons(self):
-        b0 = Button(image=self.img0, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b0 = Button(image=self.img0, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b0.place(x=139, y=461, width=81, height=23)
 
-        b1 = Button(image=self.img1, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b1 = Button(image=self.img1, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b1.place(x=323, y=249, width=92, height=22)
 
-        b2 = Button(image=self.img2, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b2 = Button(image=self.img2, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b2.place(x=513, y=249, width=97, height=23)
 
-        b3 = Button(image=self.img3, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b3 = Button(image=self.img3, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b3.place(x=699, y=249, width=90, height=23)
 
-        b4 = Button(image=self.img4, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b4 = Button(image=self.img4, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b4.place(x=327, y=461, width=81, height=23)
 
-        b5 = Button(image=self.img5, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b5 = Button(image=self.img5, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b5.place(x=513, y=461, width=81, height=23)
 
-        b6 = Button(image=self.img6, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b6 = Button(image=self.img6, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b6.place(x=886, y=247, width=81, height=23)
 
-        b7 = Button(image=self.img7, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b7 = Button(image=self.img7, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b7.place(x=699, y=461, width=81, height=23)
 
-        b8 = Button(image=self.img8, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b8 = Button(image=self.img8, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b8.place(x=886, y=461, width=81, height=23)
 
-        b9 = Button(image=self.img9, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b9 = Button(image=self.img9, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b9.place(x=132, y=249, width=101, height=23)
 
-        b10 = Button(image=self.img10, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat")
+        b10 = Button(image=self.img10, borderwidth=0, highlightthickness=0, command=self.btn_clicked, relief="flat", master=self)
         b10.place(x=841, y=663, width=108, height=23)
 
     def create_entries(self):
